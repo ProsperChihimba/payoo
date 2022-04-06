b3d1049c562fc3ae93e65afdcbaa428f9864e371
ProsperChihimba <prosperchihimba@gmail.com>
2022-04-07 01:10:48 +0300

    Sending request to the API for buying airtime

--- screens/AirtimeScreen.tsx
+++ screens/AirtimeScreen.tsx
@@ -2,12 +2,54 @@ import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Pressable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
     
 
 export default function AirtimeScreen({ navigation }: RootTabScreenProps<'Home'>) {
+
+
+    const [number, setNumber] = useState('');
+    const [amount, setAmount] = useState('');
+
+    const  buyAirtime = async ({amount, number}: {amount: number, number: string }) => {
+    try {
+
+        const response = await fetch('http://ec2-18-222-127-236.us-east-2.compute.amazonaws.com/airtime/', {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                client_email: "prosperchihimba@gmail.com",
+                phone_number: number,
+                amount: amount,
+            })
+        });
+        const content = await response.json();
+
+        console.warn(content);
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+    const sendRequest = () => {
+        if (parseInt(amount) < 500) {
+            console.warn('Low amount')
+            return
+        } else {
+            const data = {
+                amount: parseInt(amount),
+                number: number,
+            }
+            buyAirtime(data)
+        }
+    }
 
 
     
@@ -31,6 +73,8 @@ export default function AirtimeScreen({ navigation }: RootTabScreenProps<'Home'>
                     <View style={{width: '100%', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 15}}>
                     <TextInput
                         placeholder='Amount'
+                        value={amount}
+                        onChangeText={amount => setAmount(amount)}
                         keyboardType="numeric"
                         style={{ width: '90%', height: 50, borderRadius: 10, borderWidth: 1,  paddingLeft: 20 }}
                     />
@@ -39,13 +83,18 @@ export default function AirtimeScreen({ navigation }: RootTabScreenProps<'Home'>
                 <View style={{width: '100%', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 15, paddingVertical: 30}}>
                     <TextInput
                         placeholder='Phone number'
+                        value={number}
+                        onChangeText={number => setNumber(number)}
                         keyboardType="numeric"
                         style={{ width: '90%', height: 50, borderRadius: 10, borderWidth: 1,  paddingLeft: 20 }}
                     />
                 </View>
 
                 <View style={{width: '90%', alignItems: 'center'}}>
-                <TouchableOpacity style={styles.payButton}>
+                    <TouchableOpacity
+                        style={styles.payButton}
+                        onPress={() => sendRequest()}
+                    >
                         <Text style={{fontWeight: 'bold', color: 'white', textAlign: 'center'}}>Buy Airtime </Text>
                 </TouchableOpacity>
             </View>
